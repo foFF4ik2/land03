a405c14a32b9a3c2ec3cd8d43bb0d37581ff342d
Volodymyr <foFF4ik2@gmail.com>
2022-12-29 09:35:27 +0200

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        arrows: true,
+        dots: false,
+        centerPadding: '70px'
+    });
+    });
